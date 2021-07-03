410fef3d4f06c23cb858f4bb5dece74d3b7889f6
NagahamaTakahiro <nnarannnara27@gmail.com>
2021-07-03 16:45:50 +0900

    first-commit

---
+++ content-contribution.php
@@ -0,0 +1,13 @@
+          <article class="article-card">
+            <a class="card-link" href="<?php echo get_term_link($term); ?>">
+              <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-page-dummy.png" /></div>
+              <div class="body">
+                <time><?php the_time( 'Y.m.d' ); ?></time>
+                <p class="title"><?php echo $term->name; ?></p>
+                <p class="excerot"><?php echo $term->discription; ?></p>
+                <div class="buttonBox">
+                  <button type="button" class="seeDetail">MORE</button>
+                </div>
+              </div>
+            </a>
+          </article>
